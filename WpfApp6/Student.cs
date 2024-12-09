8234e8031c1d6947192d83003ab3591c514913f1
4b2g0107@office.stust.edu.tw <4b2g0107@office.stust.edu.tw>
2024-12-09 20:09:43 +0800

    完成上課內容 新增class student 新增學生列表

---
+++ WpfApp6/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    class Student
+    {
+        public string StudentId { get; set; }
+        public string StudentName{ get; set; }
+
+        public override string ToString()
+        {
+            return $"{StudentId} {StudentName}";
+        }
+        
+    }
+}
